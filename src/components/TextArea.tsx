5c413c5c87de960fa4aa7318c87d2ce42a4edf56
m2mt-l <ta28ma2@gmail.com>
2022-10-26 23:36:53 +0000

    add handleChange

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { ChangeEvent, FC, useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
@@ -7,8 +7,22 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import { TweetInfo } from "../model/tweetInfo";
+import { iconImageUrl } from "../data/iconImageUrl";
 
 const TextArea: FC = () => {
+    const [tweetInfo, setTweetInfo] = useState<TweetInfo>({
+        name: "",
+        id: "",
+        content: "",
+        icon: iconImageUrl,
+    });
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+        const { name, value } = event.target;
+        console.log(value);
+        setTweetInfo({ ...tweetInfo, [name]: value });
+    };
+
     return (
         <div>
             <h3>Create Tweet</h3>
@@ -25,8 +39,22 @@ const TextArea: FC = () => {
                     borderColor: "grey.500",
                 }}
             >
-                <TextField id="Name" label="Name" variant="outlined" sx={{ mx: 1, width: "90%" }} />
-                <TextField id="ID" label="ID" variant="outlined" sx={{ mx: 1, width: "90%" }} />
+                <TextField
+                    id="Name"
+                    label="Name"
+                    variant="outlined"
+                    sx={{ mx: 1, width: "90%" }}
+                    onChange={handleChange}
+                    defaultValue={tweetInfo.name}
+                />
+                <TextField
+                    id="ID"
+                    label="ID"
+                    variant="outlined"
+                    sx={{ mx: 1, width: "90%" }}
+                    onChange={handleChange}
+                    defaultValue={tweetInfo.id}
+                />
                 <TextField
                     id="Tweet Content"
                     label="Tweet Content"
@@ -34,13 +62,20 @@ const TextArea: FC = () => {
                     multiline
                     rows={4}
                     sx={{ mx: 1, width: "90%" }}
+                    onChange={handleChange}
+                    defaultValue={tweetInfo.content}
                 />
                 <FormControl sx={{ mx: 1, width: "90%" }}>
                     <InputLabel id="demo-simple-select-label">Icon</InputLabel>
-                    <Select labelId="demo-simple-select-label" id="demo-simple-select" label="Age">
-                        <MenuItem value={10}>Twitter</MenuItem>
-                        <MenuItem value={20}>Recursion</MenuItem>
-                        <MenuItem value={30}>Original</MenuItem>
+                    <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        label="Age"
+                        defaultValue={tweetInfo.icon.Twitter}
+                    >
+                        <MenuItem value="Twitter">Twitter</MenuItem>
+                        <MenuItem value="Recursion">Recursion</MenuItem>
+                        <MenuItem value="Original">Original</MenuItem>
                     </Select>
                 </FormControl>
                 <Button variant="contained">Create Tweet</Button>
