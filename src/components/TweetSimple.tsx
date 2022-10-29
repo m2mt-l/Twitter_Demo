720611710dc5b0ba1a38bffe2ba5b30eaba4c550
m2mt-l <ta28ma2@gmail.com>
2022-10-29 08:43:00 +0000

    style: TweetSimple

--- src/components/TweetSimple.tsx
+++ src/components/TweetSimple.tsx
@@ -2,6 +2,11 @@ import React, { FC } from "react";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import LinearScaleIcon from "@mui/icons-material/LinearScale";
+
 import { TweetInfo } from "../model/tweetInfo";
 
 const TweetSimple: FC<TweetInfo> = (props) => {
@@ -9,7 +14,47 @@ const TweetSimple: FC<TweetInfo> = (props) => {
         <div>
             <h3>Simple</h3>
             <Card variant="outlined">
-                <CardContent sx={{ pb: 0 }}></CardContent>
+                <CardContent sx={{ pb: 0, width: "900px" }}>
+                    <Box
+                        sx={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                        }}
+                    >
+                        <Box
+                            sx={{
+                                display: "flex",
+                                justifyContent: "between",
+                                alignItems: "center",
+                            }}
+                        >
+                            <Stack direction="row">
+                                <Typography
+                                    sx={{ fontSize: 15, fontWeight: "bold", mr: 1 }}
+                                    component="div"
+                                >
+                                    {props.name}
+                                </Typography>
+                                <Typography
+                                    sx={{ fontSize: 15, textAlign: "start" }}
+                                    color="text.secondary"
+                                >
+                                    {props.id}
+                                </Typography>
+                                <span style={{ fontSize: 15 }}>・</span>
+                                <Typography sx={{ fontSize: 15 }} color="text.secondary">
+                                    date
+                                </Typography>
+                            </Stack>
+                        </Box>
+                        <IconButton aria-label="liner-scale" size="small">
+                            <LinearScaleIcon fontSize="small" />
+                        </IconButton>
+                    </Box>
+                    <Typography sx={{ textAlign: "start", fontSize: 15, whiteSpace: "pre-line" }}>
+                        {props.content}
+                    </Typography>
+                </CardContent>
             </Card>
         </div>
     );
